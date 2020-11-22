0723c8f932468a863b0f4e589d3d9df196cbc1e8
shravanrn@gmail.com <shravanrn@gmail.com>
2020-11-22 10:14:34 -0800

    Free any return slot memory on sbx destruction

--- include/rlbox_lucet_sandbox.hpp
+++ include/rlbox_lucet_sandbox.hpp
@@ -510,7 +510,12 @@ protected:
     impl_create_sandbox(lucet_module_path, external_loads_exist, allow_stdio);
   }
 
-  inline void impl_destroy_sandbox() { lucet_drop_module(sandbox); }
+  inline void impl_destroy_sandbox() {
+    if (return_slot_size) {
+      impl_free_in_sandbox(return_slot);
+    }
+    lucet_drop_module(sandbox);
+  }
 
   template<typename T>
   inline void* impl_get_unsandboxed_pointer(T_PointerType p) const
